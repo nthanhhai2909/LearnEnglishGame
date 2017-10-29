63d952599ed8eff98f4a19a52cc0a943a1e799b4
Hai Nguyen Thanh <nthanhhai2909@gmail.com>
2017-10-30 03:19:44 +0700

    update giao dien lan 1

--- app/src/main/java/com/example/nthan/learnenglishgame/PlayGameActivity.java
+++ app/src/main/java/com/example/nthan/learnenglishgame/PlayGameActivity.java
@@ -1,13 +1,39 @@
 package com.example.nthan.learnenglishgame;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PlayGameActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar_playgameactivity);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_playgameactivity, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
